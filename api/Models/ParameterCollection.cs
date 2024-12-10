15c54b8a655d61f8d15e9d2b683782c03c87b22f
ericbr0wne <ericbrowne@hotmail.com>
2024-12-10 21:46:16 +0100

    finally fixed the json parsing into the correct models

--- api/Models/ParameterCollection.cs
+++ api/Models/ParameterCollection.cs
@@ -10,27 +10,20 @@ namespace api.Models
         public string Unit { get; set; }
     }
 
+    public class Station
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+
+    }
+
     public class Period
     {
         public string Key { get; set; }
         public long From { get; set; }
         public long To { get; set; }
         public string Summary { get; set; }
-    }
-
-    public class Link
-    {
-        public string Href { get; set; }
-        public string Rel { get; set; }
-        public string Type { get; set; }
-    }
-
-    public class StationData
-    {
-        public long Date { get; set; }
-        public double Value { get; set; }
-        public string Quality { get; set; }
-        public string Depth { get; set; }
     }
 
     public class Position
@@ -41,17 +34,19 @@ namespace api.Models
         public double Longitude { get; set; }
     }
 
-    public class Station
+    public class Link
     {
-        public string Key { get; set; }
-        public string Name { get; set; }
-        public string Owner { get; set; }
+        public string Href { get; set; }
+        public string Rel { get; set; }
+        public string Type { get; set; }
+    }
 
-        // The "position" array corresponds to List<Position>
-        public List<Position> Position { get; set; }
-
-        // The "value" array corresponds to List<StationData>
-        public List<StationData> Value { get; set; }
+    public class Value
+    {
+        public long Date { get; set; }
+        public double value { get; set; }
+        public string Quality { get; set; }
+        public string Depth { get; set; }
     }
 
     // Represents the full SmhiResponse in the JSON
@@ -59,12 +54,10 @@ namespace api.Models
     {
         public long Updated { get; set; }
         public Parameter Parameter { get; set; }
-        public Period Period { get; set; }
-
-        // The "station" is a single object, so it should be a single Station object
         public Station Station { get; set; }
-
-        // The "link" array corresponds to List<Link>
+        public Period Period { get; set; }
+        public List<Position> Position { get; set; }
         public List<Link> Link { get; set; }
+        public List<Value> value { get; set; }
     }
 }
