0f003a5d8e8525d5674a2897e92807ae6a17e73f
Eric Browne <ericbrowne@hotmail.com>
2025-07-08 13:14:34 +0200

    changed h2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,8 +37,7 @@ const SmhiData = () => {
 return (
   <div>
     <h1>Seafinch</h1>
-    <h2>Real-time Ocean Current</h2>
-    <p>This data comes from SMHI's open API, showing real-time ocean current measurements from monitoring station in the Baltic Sea.</p>
+    <h2>This data comes from SMHI's open API, showing real-time ocean current measurements from monitoring station in the Baltic Sea.</h2>
     <FishAnimation currentSpeed={currentItem.value} />
     <DataDisplay item={currentItem} />
     <NavigationButtons
